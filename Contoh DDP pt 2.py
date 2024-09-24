e4b73a9f3f80ef736d1f574b4b06e5c8675b70e5
KimHajinnn <theomnicient.no.0@gmail.com>
2024-09-24 14:10:11 +0800

    Add files via upload

---
+++ Contoh DDP pt 2.py
@@ -0,0 +1,30 @@
+#Umur = int(input("Masukkan Umur :"))
+
+#if Umur >= 30 :
+    #print("Selamat pagi pak/bu")
+#elif Umur >= 20 :
+    #print("Selamat pagi Bang/Mba")
+#elif Umur >= 1 :
+    ##print("Selamat pagi")
+#else :
+    #print("Errorr!!")
+
+
+#for char in "Haloooo" :
+    #print(char)
+
+#for angka in range(5):
+    #print(angka)
+
+#fruits =  ("Apple", "Banana", "Cherry")
+#print(fruits)
+#for fruit in fruits : 
+    #if fruit == "Banana" :
+        #print(fruit)
+
+
+i = 2
+while i > 6 :
+    print(i)
+
+i += (6)
